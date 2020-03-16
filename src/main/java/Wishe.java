b4020d2a90c6382b8c7b1781caa9be2c0063d466
OlgaNikulina <nikulinaov@inbox.ru>
2020-03-17 00:26:56 +0500

    vv1

---
+++ src/main/java/Wishe.java
@@ -0,0 +1,29 @@
+public class Wishe {
+    public boolean isWantMailing() {
+        return wantMailing;
+    }
+
+    public void setWantMailing(boolean wantMailing) {
+        this.wantMailing = wantMailing;
+    }
+
+    public boolean isWantOnLecture() {
+        return wantOnLecture;
+    }
+
+    public void setWantOnLecture(boolean wantOnLecture) {
+        this.wantOnLecture = wantOnLecture;
+    }
+
+    public boolean isWantTest() {
+        return wantTest;
+    }
+
+    public void setWantTest(boolean wantTest) {
+        this.wantTest = wantTest;
+    }
+
+    private boolean wantMailing;
+    private boolean wantOnLecture;
+    private boolean wantTest;
+}
