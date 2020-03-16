f5f6f7c319132160a3a12a2190455f51852d4272
OlgaNikulina <nikulinaov@inbox.ru>
2020-03-17 01:18:21 +0500

    v2

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,9 +1,24 @@
+import sun.jvm.hotspot.types.CIntegerField;
+
 public class Wall {
     private int id;
-    private boolean read;
-    private boolean like;
-    private boolean share;
-    private boolean canComments;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
 
     public int getId() {
         return id;
@@ -13,35 +28,142 @@ public class Wall {
         this.id = id;
     }
 
-    public boolean isRead() {
-        return read;
+    public int getOwnerId() {
+        return ownerId;
     }
 
-    public void setRead(boolean read) {
-        this.read = read;
+    public void setOwnerId(int owner_id) {
+        this.ownerId = owner_id;
     }
 
-    public boolean isLike() {
-        return like;
+    public int getFromId() {
+        return fromId;
     }
 
-    public void setLike(boolean like) {
-        this.like = like;
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
     }
 
-    public boolean isShare() {
-        return share;
+    public int getCreatedBy() {
+        return createdBy;
     }
 
-    public void setShare(boolean share) {
-        this.share = share;
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
     }
 
-    public boolean isCanComments() {
-        return canComments;
+    public int getDate() {
+        return date;
     }
 
-    public void setCanComments(boolean canComments) {
-        this.canComments = canComments;
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public void setFriendsOnly(int friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public String getPostType() {
+        return postType;
+    }
+
+    public void setPostType(String postType) {
+        this.postType = postType;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getCanPin() {
+        return canPin;
+    }
+
+    public void setCanPin(int canPin) {
+        this.canPin = canPin;
+    }
+
+    public int getCanDelete() {
+        return canDelete;
+    }
+
+    public void setCanDelete(int canDelete) {
+        this.canDelete = canDelete;
+    }
+
+    public int getCanEdit() {
+        return canEdit;
+    }
+
+    public void setCanEdit(int canEdit) {
+        this.canEdit = canEdit;
+    }
+
+    public int getIsPinned() {
+        return isPinned;
+    }
+
+    public void setIsPinned(int isPinned) {
+        this.isPinned = isPinned;
+    }
+
+    public int getMarkedAsAds() {
+        return markedAsAds;
+    }
+
+    public void setMarkedAsAds(int markedAsAds) {
+        this.markedAsAds = markedAsAds;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
     }
 }
+
+
+
